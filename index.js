e5fb71c600e7e5fdf5687b42bf2e6fa9aa962412
unknown <danniqueeg@gmail.com>
2021-11-16 11:59:38 +0100

    added express server and procfile

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
+const express = require("express");
+const app = express();
 
 
-$(window).scroll(() => {
+app.use(express.urlencoded({
+    extended: true
+  }));
+  
+  app.use(express.static("public"));
 
-    $('#pricing').each( function(){
-        const el               = $(this),
-              top_of_section   = el.offset().top,
-              bottom_of_window = $(window).scrollTop() + $(window).height() + 100;
 
-        if( bottom_of_window > top_of_section ) {
-            el.addClass('slideItUp').removeClass('slideItDown');
-        } else {
-            el.addClass('slideItDown').removeClass('slideItUp');
-        }
-    }); 
-
-    $('#makeitslide').each( function(){
-        const el               = $(this),
-              top_of_section   = el.offset().top,
-              bottom_of_window = $(window).scrollTop() + $(window).height() + 100;
-
-        if( bottom_of_window > top_of_section ) {
-            el.addClass('wrapperin').removeClass('wrapperout');
-        } else {
-            el.addClass('wrapperout').removeClass('wrapperin');
-        }
-    }); 
-
+  app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+    res.send()
 });
+
+app.listen(3000, () => {
+    console.log("Server port 3000 is on");
+  })
+
+
+// $(window).scroll(() => {
+
+//     $('#pricing').each( function(){
+//         const el               = $(this),
+//               top_of_section   = el.offset().top,
+//               bottom_of_window = $(window).scrollTop() + $(window).height() + 100;
+
+//         if( bottom_of_window > top_of_section ) {
+//             el.addClass('slideItUp').removeClass('slideItDown');
+//         } else {
+//             el.addClass('slideItDown').removeClass('slideItUp');
+//         }
+//     }); 
+
+//     $('#makeitslide').each( function(){
+//         const el               = $(this),
+//               top_of_section   = el.offset().top,
+//               bottom_of_window = $(window).scrollTop() + $(window).height() + 100;
+
+//         if( bottom_of_window > top_of_section ) {
+//             el.addClass('wrapperin').removeClass('wrapperout');
+//         } else {
+//             el.addClass('wrapperout').removeClass('wrapperin');
+//         }
+//     }); 
+
+// });
+
+
